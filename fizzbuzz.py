f56ae9c109e7596f4cb24a2aa8d2afb13110f7e1
David Duirs <duirsdevelopment@gmail.com>
2024-04-28 16:48:24 +0200

    fizzbuzz game

---
+++ fizzbuzz.py
@@ -0,0 +1,18 @@
+# Write a program to output the first 100 FizzBuzz numbers
+
+# if the number is divisible by 3 print Fizz
+# if the number is divisible by 5 print Buzz
+# if the number is divisible by 3 and 5 print FizzBuzz
+# if the number is not divisible by 3, 5 or both, print number
+
+x=1
+while x < 100:
+    if (x % 3) == 0 and (x % 5) == 0:
+        print("FizzBuzz")
+    elif (x % 3) == 0:
+        print("Fizz")
+    elif (x % 5) == 0:
+        print("Buzz")
+    else:
+        print (x)
+    x = x+1
